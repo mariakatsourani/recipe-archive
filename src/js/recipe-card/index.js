c2a314218765b5613c2b6953e0c1c812969e5d3c
Maria Katsourani <maria.katsourani@outlook.com>
2018-08-08 22:48:15 +0200

    Add all elements to custom component

--- src/js/recipe-card/index.js
+++ src/js/recipe-card/index.js
@@ -1,3 +1,5 @@
+import { recipeCardStyle } from './style';
+
 class RecipeCard extends HTMLElement {
   constructor() {
     super();
@@ -6,29 +8,56 @@ class RecipeCard extends HTMLElement {
 
     var wrapper = document.createElement('span');
     
+    
+    const style = document.createElement('style');
+    style.textContent = recipeCardStyle;
+    
+    // attach the created elements to the shadow dom
+    shadow.appendChild(style);
+    shadow.appendChild(wrapper);
+
     //title
     const h2 = document.createElement('h2');
     const title = this.getAttribute('title');
     h2.textContent = title;
+    wrapper.appendChild(h2);
 
     //body
     const body = document.createElement('div');
+    body.setAttribute('class','body');
     const bodyText = this.getAttribute('body');
     body.textContent = bodyText;
+    wrapper.appendChild(body);
 
     //cooking time
     const ct = document.createElement('span');
-    ct.setAttribute('class','badge border border-purple');
+    ct.setAttribute('class','badge cooking-time');
     const mins = this.getAttribute('cooking-time');
     ct.textContent = mins;
-
-    // attach the created elements to the shadow dom
-
-    shadow.appendChild(wrapper);
-
-    wrapper.appendChild(h2);
-    wrapper.appendChild(body);
     wrapper.appendChild(ct);
+
+    //categories
+    const categoriesRaw = this.getAttribute('categories');
+    const categoriesArray = categoriesRaw.split(' ');
+
+    categoriesArray.map(s => {
+      const category = document.createElement('span');
+      category.setAttribute('class','badge category');
+      category.textContent = s;
+      wrapper.appendChild(category);
+    });
+
+    //tags
+    const tagsRaw = this.getAttribute('tags');
+    const tagsArray = tagsRaw.split(' ');
+
+    tagsArray.map(s => {
+      const tag = document.createElement('span');
+      tag.setAttribute('class','badge tag');
+      tag.textContent = s;
+      wrapper.appendChild(tag);
+    });
+
   }
 }
 
